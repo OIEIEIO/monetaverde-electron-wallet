4a1ecd4658300d0fbfb29bc2c7fdfa0e9d0ea9b7
OIEIEIO <oieieio@protonmail.com>
2019-12-14 16:00:16 -0500

    Update daemon.js

--- src-electron/main-process/modules/daemon.js
+++ src-electron/main-process/modules/daemon.js
@@ -20,8 +20,8 @@ export class Daemon {
 
         // Settings for timestamp to height conversion
         // These are initial values used to calculate the height
-        this.PIVOT_BLOCK_HEIGHT = 150000
-        this.PIVOT_BLOCK_TIMESTAMP = 1554667008
+        this.PIVOT_BLOCK_HEIGHT = 26925
+        this.PIVOT_BLOCK_TIMESTAMP = 1576357029
         this.PIVOT_BLOCK_TIME = 60
     }
 
